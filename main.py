629816fff41da28494c1dadadfc4d20b9f51e499
Redwan Newaz <redwan06me@gmail.com>
2024-09-10 13:21:23 -0500

    fixed

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def get_video_frames(name):
     video.release()
     cv2.destroyAllWindows()
 
-def track_object(frames):
+def track_object(frames, numTrackers):
 
     roiFrames = list(frameTracker.keys())
+    prevStates = []
     for i, frame in enumerate(frames):
         if i in roiFrames :
             # bbox = (1647, 436, 119, 65)
@@ -37,11 +38,17 @@ def track_object(frames):
             success, bbox = tracker.update(frame)
 
             if success:
+                center = (int(bbox[0] + bbox[2] / 2), int(bbox[1] + bbox[3] / 2))
+                prevStates.append(center)
                 # Draw bounding box
                 x, y, w, h = [int(i) for i in bbox]
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.putText(frame, "Tracking", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                cv2.putText(frame, "Quadrotor", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
+        # show previous trajectory
+        for center in prevStates[-numTrackers:]:
+            # Draw circle at center point
+            cv2.circle(frame, center, 10, (0, 255, 0), -1)
         # Display result
         cv2.imshow("Tracking", frame)
 
@@ -54,7 +61,7 @@ def track_object(frames):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    frames = get_video_frames('C:/Users/robor/Downloads/PXL_20240724_183502779.mp4')
-    track_object(frames)
+    frames = get_video_frames('/home/airlab/GDrive/ResearchWorkspace/Bow-ICRA-2025/Video/BOW(Drone)/PXL_20240724_183502779.mp4')
+    track_object(frames, numTrackers=30)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
